443cfb395487ca2919d301e1ecd3b3c7bad8b949
Luiz Roberto <passageiro1829@gmail.com>
2019-12-18 13:34:41 -0300

    adicionando templstes com supervisao do controlador

---
+++ src/main/java/br/edu/ifal/qualquer_lugar/Controller.java
@@ -0,0 +1,20 @@
+package br.edu.ifal.qualquer_lugar;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Controller{
+
+    @RequestMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView("index.html");
+    }
+
+    @RequestMapping("/menu")
+    public ModelAndView menu(){
+        return new ModelAndView("menu.html");
+    }
+
+}
